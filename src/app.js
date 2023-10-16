4bfb608c4a8b474817de8c69afa9f04f4d1ee9da
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2023-10-16 21:15:23 +0000

    js domainNames

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let domainNames = [];
+
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+
+      domainNames.push(pronoun[i] + adj[j] + noun[k] + '.com');
+    }
+  }
+}
+
+
+console.log(domainNames);
